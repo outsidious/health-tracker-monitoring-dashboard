7b1b3010b0eb90f5bb9c4ba4cd452f89b0358c3a
Artem <artem2000vlas@mail.ru>
2020-07-10 10:34:37 +0300

    style fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   maps: {
-        street_title: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        wiki_title: 'http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
-        attribute: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }
+    street_title: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    wiki_title: 'http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
+    attribute: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   maps: {
-        street_title: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        wiki_title: 'http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
-        attribute: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }
+    street_title: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    wiki_title: 'http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
+    attribute: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }
 };
 
 /*
