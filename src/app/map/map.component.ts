e7357abf9c7d0f3cb20fb9a38fd75b51c0946298
Artem <artem2000vlas@mail.ru>
2020-07-16 21:10:52 +0300

    time added to component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -48,34 +48,20 @@ export class MapComponent {
     ) {}
 
     onMapReady(map) {
-        // get a local reference to the map as we need it later
         this.map = map;
         this.addMarkers();
     }
 
     addMarkers() {
-        // simply iterate over the array of markers from our data service
-        // and add them to the map
         this.dataService.getMarkers().subscribe(data => {
             this.markers=data["locationList"];
+            //console.log(this.markers);
+            let currentTime = new Date().toISOString();
+            let currentTimeMseconds = Date.parse(currentTime);
+            //console.log(currentTimeMseconds);
             for (const entry of this.markers) {
-                // dynamically instantiate a HTMLMarkerComponent
-                /*
-                const factory = this.resolver.resolveComponentFactory(
-                    MarkerComponent
-                );
-    
-                // we need to pass in the dependency injector
-                const component = factory.create(this.injector);
-    
-                // wire up the @Input() or plain variables (doesn't have to be strictly an @Input())
-                component.instance.data = entry;
-    
-                // we need to manually trigger change detection on our in-memory component
-                // s.t. its template syncs with the data we passed in
-                component.changeDetectorRef.detectChanges();
-                */
-                // create a new Leaflet marker at the given position
+                let MarkerTimeMseconds = Date.parse(entry.timeStamp);
+                //console.log(MarkerTimeMseconds);
                 let m = marker(entry.currentValue, {
                     icon: icon({
                         iconSize: [25, 41],
@@ -125,7 +111,7 @@ export class MapComponent {
     // Set the initial set of displayed layers
     options = {
         layers: [this.streetMaps],
-        zoom: 8,
-        center: latLng([46.879966, -121.726909]),
+        zoom: 3,
+        center: latLng([20.000, 50.000]),
     };
 }
--- src/app/marker/http_marker.component.ts
+++ src/app/marker/http_marker.component.ts
@@ -7,6 +7,7 @@ import { LatLngExpression } from "leaflet";
 })
 export class HttpMarkerComponent {
     deviceId: String;
-    sensorType: String;
     currentValue: LatLngExpression;
+    timeStamp: string;
 }
+ 
